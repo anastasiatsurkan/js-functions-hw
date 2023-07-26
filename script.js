7194a1cf0dd3a49cc785236d9db3c4b12fe812af
Anastasia Turcan <anastasiaturcan@bk.ru>
2023-07-26 13:21:05 +0300

    sarcina 1 - done

---
+++ script.js
@@ -0,0 +1,17 @@
+let n1 = 5;
+let n2 = 3;
+let n3 = 11;
+
+if (n1 >= n2 && n1 >= n3) {
+    console.log("Valoarea maxima este n1.");
+
+} else if (n2 >= n1 && n2 >= n3) {
+    console.log("Valoarea maxima este n2.");
+
+} else {
+    console.log("Valoarea maxima este n3.");
+}
+
+
+
+
