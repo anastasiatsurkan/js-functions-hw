dce3b3828cd50a6ac2dfaa7f85ffbff28d32e6ac
Anastasia Turcan <anastasiaturcan@bk.ru>
2023-07-26 14:00:50 +0300

    modificari - adaugare function & return

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 // sarcina 1
-/*let n1 = 5;
-let n2 = 3;
-let n3 = 11;
+/*function maxValue(n1, n2, n3) {
+    if (n1 >= n2 && n1 >= n3) {
+        return ("Valoarea maxima este n1.");
 
-if (n1 >= n2 && n1 >= n3) {
-    console.log("Valoarea maxima este n1.");
+    } else if (n2 >= n1 && n2 >= n3) {
+        return ("Valoarea maxima este n2.");
 
-} else if (n2 >= n1 && n2 >= n3) {
-    console.log("Valoarea maxima este n2.");
+    } else {
+        return ("Valoarea maxima este n3.");
+    }
+}
 
-} else {
-    console.log("Valoarea maxima este n3.");
-}*/
+console.log(maxValue(13, 8, 4));*/
 
 // sarcina 2
-/*let n1 = 5;
-let n2 = 3;
-let n3 = 11;
+/*function minValue(n1, n2, n3) {
+    if (n1 <= n2 && n1 <= n3) {
+        return ("Valoarea minima este n1.");
 
-if (n1 <= n2 && n1 <= n3) {
-    console.log("Valoarea minima este n1.");
+    } else if (n2 <= n1 && n2 <= n3) {
+        return ("Valoarea minima este n2.");
 
-} else if (n2 <= n1 && n2 <= n3) {
-    console.log("Valoarea minima este n2.");
+    } else {
+        return ("Valoarea minima este n3.");
+    }
+}
 
-} else {
-    console.log("Valoarea minima este n3.");
-}*/
+console.log(minValue(20, 5, 11));*/
 
 // sarcina 3
-/*let varsta = 21;
+/*function varstaPersoanei(number) {
+    if (number >= 18) {
+        return ("Ești potrivit pentru a juca acest joc");
 
-if (varsta >= 18) {
-    console.log("Ești potrivit pentru a juca acest joc");
+    } else {
+        return ("Ești prea tânăr pentru a juca acest joc");
+    }
+}
 
-} else {
-    console.log("Ești prea tânăr pentru a juca acest joc");
-}*/
+console.log(varstaPersoanei(21));*/
+
 
 // sarcina 4
 /*function numberLimits(number) {
